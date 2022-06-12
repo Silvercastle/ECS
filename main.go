4f5438a0041de7b0c57242c26226093f68fd63de
김은성 <iadus@gitauto.com>
2022-06-12 16:48:26 +0900

    소스커밋

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func main() {
+
+	e := echo.New()
+
+	e.GET("/hello", hello)
+
+	e.GET("/ping", ping)
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, world!!!")
+}
+
+func ping(c echo.Context) error {
+	return c.String(http.StatusOK, "pong")
+}
